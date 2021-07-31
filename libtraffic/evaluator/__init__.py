e3f18c1cbf31d9e2a7b0abea68890a7e58ca9b3d
jjw <jwjiang_buaa@163.com>
2021-07-31 19:17:48 +0800

    add code

---
+++ libtraffic/evaluator/__init__.py
@@ -0,0 +1,9 @@
+from libtraffic.evaluator.traj_loc_pred_evaluator import TrajLocPredEvaluator
+from libtraffic.evaluator.traffic_state_evaluator import TrafficStateEvaluator
+from libtraffic.evaluator.cara_loc_pred_evaluator import CARALocPredEvaluator
+
+__all__ = [
+    "TrajLocPredEvaluator",
+    "TrafficStateEvaluator",
+    "CARALocPredEvaluator",
+]
